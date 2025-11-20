bc1c0379bf13772d228ea8f4f3eeec32ab7c565e
YudhishtarCharan <yudhishtar.charan.98@gmail.com>
2025-11-20 22:17:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ with col2:
 input_data = [sepal_length, sepal_width, petal_length, petal_width]
 
 # ✅ Local Flask URL instead of Render
-FLASK_URL = "http://127.0.0.1:5000/predict"
+FLASK_URL = "https://ns1-ryg7.onrender.com/predict"
 
 # Make prediction via Flask API
 try:
